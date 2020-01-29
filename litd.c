e7727c8f9e6b68b44aead69eb5311f8dafb355cf
hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2020-01-29 21:38:41 +0100

    Added signals handler

--- litd.c
+++ litd.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include <time.h>
+#include <signal.h>
+#include <stdlib.h>
 #include "xidle.h"
 
 #define SENSOR_PATH "/sys/devices/platform/applesmc.768/light"
 #define DISP_PATH "/sys/class/backlight/intel_backlight/brightness"
-#define DISP_ACT_PATH "/sys/class/backlight/intel_backlight/actual_brightness"
 #define KEY_PATH "/sys/devices/platform/applesmc.768/leds/smc::kbd_backlight/brightness"
+
+void handle_signal(int sig) {
+    printf("Catched signal: %d\n", sig);
+    exit(1);
+}
+
+void prepare_signals(void) {
+    signal(SIGABRT, handle_signal);
+    signal(SIGHUP, handle_signal);
+    signal(SIGINT, handle_signal);
+    signal(SIGQUIT, handle_signal);
+    signal(SIGTERM, handle_signal);
+}
+
+void nsleep(int milisec) {
+    struct timespec req = {0};
+    req.tv_sec = 0;
+    req.tv_nsec = milisec * 1000000L;
+    nanosleep(&req, NULL);
+    return;
+}
 
 int get_file_value(const char *path) {
     FILE *fp = NULL;
@@ -22,7 +44,12 @@ int get_file_value(const char *path) {
 
 int main(void) {
 
-    
+    prepare_signals();
+
+    while (1) {
+        printf("Keyboard: %d\n", get_file_value(KEY_PATH));
+        nsleep(400);
+    }
 
     return 0;
 }
