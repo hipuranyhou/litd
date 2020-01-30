2cf45f10c9680772009d61ff36b00c61a1a104b9
hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2020-01-30 15:27:25 +0100

    Finished ver 0.2

--- litd.c
+++ litd.c
@@ -1,7 +1,5 @@
 // Hipuranyhou - litd - ver 0.2 - 30.01.2020
 
-#include <stdio.h>
-#include <unistd.h>
 #include "xidle.h"
 #include "daemonize.h"
 #include "functions.h"
@@ -13,28 +11,6 @@
 #define POLL 400
 #define IDLE 5000
 #define RESET_MAN 3600000
-
-int process_options(int argc, char **argv, int *daemon, int *verbose) {
-    int opt;
-    // Process command line options
-    while ((opt = getopt(argc, argv, ":dv")) != -1) {
-        switch (opt) {
-            // Daemonize
-            case 'd':
-                *daemon = 1;
-                break;
-            // Verbose
-            case 'v':
-                *verbose = 1;
-                break;
-            // Unknown option
-            case '?':
-                fprintf(stderr, "Unknown option!\n");
-                return 0;
-        }
-    }
-    return 1;
-}
 
 int main(int argc, char **argv) {
 
@@ -66,8 +42,6 @@ int main(int argc, char **argv) {
 
         // Reset manual mode after RESET_MAN milliseconds
         if (reset - POLL > RESET_MAN) {
-            disp_val = get_file_value(DISP_PATH, "%d");
-            key_val = get_file_value(KEY_PATH, "%d");
             disp_val_last = disp_val;
             key_val_last = key_val;
             reset = 0;
@@ -86,23 +60,10 @@ int main(int argc, char **argv) {
         }
 
         // Calculate brightness values
-        if (disp_man && key_man) {
-            disp_val = disp_val_last;
-            key_val = key_val_last;
-        } else if (disp_man && !key_man) {
-            disp_val = disp_val_last;
-            key_val = calc_key_val(sens_val);
-            key_val_last = key_val;
-        } else if (!disp_man && key_man) {
-            disp_val = calc_disp_val(sens_val);
-            disp_val_last = disp_val;
-            key_val = key_val_last;
-        } else if (!disp_man && !key_man) {
-            disp_val = calc_disp_val(sens_val);
-            disp_val_last = disp_val;
-            key_val = calc_key_val(sens_val);
-            key_val_last = key_val;
-        }
+        disp_val = (disp_man) ? disp_val_last : calc_disp_val(sens_val);
+        key_val = (key_man) ? key_val_last : calc_key_val(sens_val);
+        disp_val_last = disp_val;
+        key_val_last = key_val;
 
         // Turn off keyboard after IDLE milliseconds
         if (idle > IDLE)
@@ -118,6 +79,7 @@ int main(int argc, char **argv) {
 
         // Wait for POLL millisecond
         nsleep(POLL);
+
     }
 
     // We never get here
