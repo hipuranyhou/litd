15992f8ea891b4efdeb47cd0c6bd0f2582804ca9
hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2020-01-31 13:05:23 +0100

    Added handler for SIGUSR1 and logging.

--- daemonize.c
+++ daemonize.c
@@ -12,16 +12,19 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <syslog.h>
 #include "daemonize.h"
 #include "functions.h"
 
 void handle_signal(int sig) {
     // Exit after every prepared signal
-    exit(1);
+    syslog (LOG_NOTICE, "Terminated.");
+    closelog();
+    exit(sig);
 }
 
 void prepare_signals(void) {
-    // Prepare all signals which we are able to handle
+    // Prepare all signals after which we terminate
     signal(SIGABRT, handle_signal);
     signal(SIGHUP, handle_signal);
     signal(SIGINT, handle_signal);
@@ -73,8 +76,11 @@ void daemonize(void) {
     for (fd = sysconf(_SC_OPEN_MAX); fd >= 0; fd--)
         close(fd);
 
-    /* Try to write PID of daemon to lockfile */
+    // Write PID of daemon to .pid file
     write_file_value("/home/hipuranyhou/.litd.pid", (int)getpid());
+
+    // Open the log file
+    openlog ("litd", LOG_PID, LOG_DAEMON);
 
     return;
 }
